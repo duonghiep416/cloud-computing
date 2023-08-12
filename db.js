b8e490ca6d2a59c9070e78fefd0537b9ef4d48c0
duonghiep416 <duonghiep416@gmail.com>
2023-08-12 20:36:46 +0700

    Update code

--- db.js
+++ db.js
@@ -1,28 +1,31 @@
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient, ObjectId } = require("mongodb");
 
-const mongoURI = 'mongodb+srv://khanhnqbh00114:khanh123@cluster0.m27xh35.mongodb.net/'; // Replace with your MongoDB connection URL
-const dbName = 'products'; // Replace with your database name
+const mongoURI =
+    "mongodb+srv://haianhngu:haianhngu@cluster0.1oekati.mongodb.net/"; // Replace with your MongoDB connection URL
+const dbName = "products"; // Replace with your database name
 
 let db;
 
 const connectToDatabase = async () => {
-  try {
-    const client = await MongoClient.connect(mongoURI, {
-      useUnifiedTopology: true,
-    });
-    db = client.db(dbName);
-    console.log('Connected to MongoDB successfully!');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-    throw err; // Throw the error to indicate a failed connection
-  }
+    try {
+        const client = await MongoClient.connect(mongoURI, {
+            useUnifiedTopology: true,
+        });
+        db = client.db(dbName);
+        console.log("Connected to MongoDB successfully!");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+        throw err; // Throw the error to indicate a failed connection
+    }
 };
 
 const getDatabase = () => {
-  if (!db) {
-    throw new Error('Database not connected. Call connectToDatabase first.');
-  }
-  return db;
+    if (!db) {
+        throw new Error(
+            "Database not connected. Call connectToDatabase first."
+        );
+    }
+    return db;
 };
 
 module.exports = { connectToDatabase, getDatabase, ObjectId };
